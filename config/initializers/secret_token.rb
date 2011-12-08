41f67e035f0ff9178d7a5929184d51ae1fe10dc3
Etienne Araya <etienne.araya@geoxite.com>
2011-12-08 11:10:31 -0300

    guardado inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8441d68745fcc3186c5b9c2156743312a60610b15e9b4f0790feccf9ba4b3a7cf8b81d5cdd8a38b1ce81bfee0c64395707fdd112c43731081091bf3a91ca500c'
